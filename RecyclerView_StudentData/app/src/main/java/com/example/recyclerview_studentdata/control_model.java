bd6ff228a8af410aaf28a26bac70991b164fdb3d
Sojnoy <sdsonjoy2003@gmail.com>
2024-10-17 16:10:35 +0600

    RecyclerView

---
+++ RecyclerView_StudentData/app/src/main/java/com/example/recyclerview_studentdata/control_model.java
@@ -0,0 +1,13 @@
+package com.example.recyclerview_studentdata;
+
+public class control_model {
+    String name;
+    String number;
+    public control_model(String name, String number){
+        this.name=name;
+        this.number=number;
+
+    }
+
+
+}
